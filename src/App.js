06457041c3710a604d521069c89bfc7909db5558
unknown <kgy424@naver.com>
2023-06-13 16:46:13 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const onSubmit = () => {
+    axios.post("http://localhost:3008/posts", {
+      title: title,
+      body: body,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="mb-3">
+        <label className="form-label">🔧 Title</label>
+        <input
+          className="form-control"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">🔨 Body</label>
+        <textarea
+          className="form-control"
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        />
+      </div>
+      <button className="btn btn-warning" onClick={onSubmit}>
+        🛠 Post 🛠
+      </button>
     </div>
   );
 }
